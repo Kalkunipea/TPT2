7e88813633fdbc3ac44845a5df149cb3af0a4c57
Tehnikumi Opilane <opilane@tptlive.ee>
2018-05-23 09:48:59 +0300

    Add project files.

---
+++ TPT2/TPT2/ViewModels/AjatabelViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPT2.ViewModels
+{
+    class AjatabelViewModel
+    {
+    }
+}
---
+++ TPT2/TPT2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TPT2.Models;
+
+namespace TPT2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
